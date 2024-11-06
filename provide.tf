8313b62504cadc16f270c1545a320d34456dfc9c
Your Name <you@example.com>
2024-11-06 21:31:09 +0530

    msg

--- provide.tf
+++ provide.tf
@@ -1,18 +1,18 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
-    }
- backend "s3" {
-    bucket = "aws-raidi-state"
-    key    = "expense"
-    region = "us-east-1"
-    dynamodb_table = "aws-locking"
-  }
-}
-provider "aws" {
-    #configuration options
-    region = "us-east-1"
-}
+# terraform {
+#     required_providers {
+#         aws = {
+#             source = "hashicorp/aws"
+#             version = "~> 5.0"
+#         }
+#     }
+#  backend "s3" {
+#     bucket = "aws-raidi-state"
+#     key    = "expense"
+#     region = "us-east-1"
+#     dynamodb_table = "aws-locking"
+#   }
+# }
+# provider "aws" {
+#     #configuration options
+#     region = "us-east-1"
+# }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable  "sg_id" {
-  type        = string
-  default     = "sg-017e60c4b82d2e66b"
-  description = "description"
-}
+# variable  "sg_id" {
+#   type        = string
+#   default     = "sg-017e60c4b82d2e66b"
+#   description = "description"
+# }
 
-variable "public_subnet_id" {
-    type = string
-    default = "subnet-04d1d2bc95f495b3f"
-}
+# variable "public_subnet_id" {
+#     type = string
+#     default = "subnet-04d1d2bc95f495b3f"
+# }
