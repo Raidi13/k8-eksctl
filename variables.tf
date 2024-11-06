269374c5995d07a59ef3810b443a83c4538ac815
Your Name <you@example.com>
2024-11-06 12:57:06 +0530

    msg

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable  "sg_id" {
   type        = string
-  default     = "sg-0e914dc599cdf119a"
+  default     = "sg-017e60c4b82d2e66b"
   description = "description"
 }
 
 variable "public_subnet_id" {
     type = string
-    default = ""
+    default = "subnet-04d1d2bc95f495b3f"
 }
