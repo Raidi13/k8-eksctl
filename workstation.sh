3a8cd916e26c4bacbdb95cbd5fad27128e6d9da0
Your Name <you@example.com>
2024-11-20 22:10:14 +0530

    msg

--- workstation.sh
+++ workstation.sh
@@ -64,3 +64,6 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 VALIDATE $? "helm installation"
+
+#k9s
+# curl -sS https://webinstall.dev/k9s | bash 
